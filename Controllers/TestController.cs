02ca16e43a17785a21a9d7ba844f6b6fa27734ef
maximuse.domn <user.maximuse@gmail.com>
2023-01-02 14:26:45 +0200

    Added test controller

---
+++ Controllers/TestController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PlannerScheduler.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        // GET: <TestController>
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello to your server!";
+        }
+
+        // GET <TestController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST <TestController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT <TestController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE /<TestController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
